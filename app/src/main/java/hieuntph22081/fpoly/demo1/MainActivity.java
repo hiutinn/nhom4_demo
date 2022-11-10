b10162045642f0b76c8a036d27afbc42db46e464
HiuTin <tienhieun5@gmail.com>
2022-11-10 13:06:07 +0700

    init project

---
+++ app/src/main/java/hieuntph22081/fpoly/demo1/MainActivity.java
@@ -0,0 +1,14 @@
+package hieuntph22081.fpoly.demo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
