cfc83775e98df13e819fca2f3a20c64b36bf9a4b
vungoc2123 <nvu806101@gmail.com>
2022-11-10 13:26:35 +0700

    add logo

---
+++ app/src/main/java/hieuntph22081/fpoly/demo1/LogoMain.java
@@ -0,0 +1,14 @@
+package hieuntph22081.fpoly.demo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogoMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo_main);
+    }
+}
